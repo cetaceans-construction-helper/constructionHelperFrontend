454a70a629d52bd0e7d8d72a08c0426c5b8d885a
SOOYEONKIM <sooyeon@10.nate.com>
2026-02-27 14:07:37 +0900

    Updates

--- assets/project-BavW_IJH.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./apiClient-Cyhc7A6k.js";const o={async getProjects(){const{data:t}=await e.get("/project/getProjectList");return t}};export{o as p};
---
+++ assets/project-DY5jMtta.js
@@ -0,0 +1,1 @@
+import{a as e}from"./apiClient-ycqybHFy.js";const o={async getProjects(){const{data:t}=await e.get("/project/getProjectList");return t}};export{o as p};
