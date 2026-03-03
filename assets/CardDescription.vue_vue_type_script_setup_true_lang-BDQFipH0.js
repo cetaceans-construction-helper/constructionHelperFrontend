ae8c44e63591892848eb59b315f4c23665fd766c
SOOYEONKIM <sooyeon@SOOYEONKIMui-MacBookPro.local>
2026-03-03 09:49:50 +0900

    Updates

---
+++ assets/CardDescription.vue_vue_type_script_setup_true_lang-BDQFipH0.js
@@ -0,0 +1,1 @@
+import{c as t}from"./utils-Dq3Si0Jd.js";import{d as a,c as r,q as n,u as c,s as p,o as l}from"./index-CHP_JJjq.js";const u=a({__name:"CardDescription",props:{class:{}},setup(s){const e=s;return(o,m)=>(l(),r("p",{"data-slot":"card-description",class:n(c(t)("text-muted-foreground text-sm",e.class))},[p(o.$slots,"default")],2))}});export{u as _};
